a27e58b1c15d504b956041d073abe525867cd410
Alexander <alex.silcorp@gmail.com>
2024-01-25 16:41:25 +0100

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
+
+function  getListOfEnvParams(prefix = "") {
+  return Object.keys(process.env).filter((paramName) => paramName.startsWith(prefix));
+}
 const parseEnv = () => {
-    // Write your code here 
+  const rssParams = getListOfEnvParams("RSS_");
+  const outputStrings = rssParams.map((paramName) => {
+    const paramValue = process.env[paramName];
+
+    return `${paramName}=${paramValue}`;
+  });
+  const outputString = outputStrings.join("; ");
+
+  process.stdout.write(outputString + "\n");
 };
 
 parseEnv();
