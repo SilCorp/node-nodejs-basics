513d4da3b3d263384b3c30cdd837a2a70a19dd80
Alexander <alex.silcorp@gmail.com>
2024-01-25 16:00:46 +0100

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const params = process.argv.slice(2);
+  const outputStrings = [];
+
+  for (let i = 0; i < params.length; i += 2) {
+    const arg = params[i];
+    const param = params[i+1];
+
+    const outputString = `${arg} is ${param}`
+    outputStrings.push(outputString);
+  }
+  const outputString = outputStrings.join(", ");
+  process.stdout.write(outputString + "\n");
 };
 
 parseArgs();
