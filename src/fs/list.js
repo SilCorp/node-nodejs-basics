a4612b540701c722ad1eef33a5062e39890460c0
Alexander <alex.silcorp@gmail.com>
2024-01-25 23:44:33 +0100

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import {fileURLToPath} from "url";
+import fs from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  const filesFolderPath = fileURLToPath(new URL("./files", import.meta.url));
+
+  try {
+    const fileNames = await fs.readdir(filesFolderPath);
+    console.log(fileNames);
+  } catch (e) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
