73394b38c16df62c23ddcbda7671d2643e5435db
Alexander <alex.silcorp@gmail.com>
2024-01-25 23:41:10 +0100

    feat: implement remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import {fileURLToPath} from "url";
+import fs from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+    const fileToRemovePath = fileURLToPath(new URL("./files/fileToRemove.txt", import.meta.url));
+
+    try {
+      await fs.unlink(fileToRemovePath);
+    } catch (e) {
+      throw new Error("FS operation failed");
+    }
 };
 
 await remove();
