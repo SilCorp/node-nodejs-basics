6314f0cc319bdc8bb955bc0720da2000f1bde095
Alexander <alex.silcorp@gmail.com>
2024-01-26 12:51:08 +0100

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import {fileURLToPath} from "url";
+import crypto from "node:crypto";
+import {createReadStream} from "node:fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = fileURLToPath(new URL("./files/fileToCalculateHashFor.txt", import.meta.url));
+
+  const readStream = createReadStream(filePath);
+  const hash = crypto.createHash("sha256");
+
+  readStream.pipe(hash).setEncoding("hex").pipe(process.stdout);
 };
 
 await calculateHash();
