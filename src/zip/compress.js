7c30f86aa9f7b2e0cab3e16e42f1ea83abdc0b23
Alexander <alex.silcorp@gmail.com>
2024-01-27 01:19:31 +0100

    feat: implement zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,16 @@
+import { createGzip } from "node:zlib";
+import {fileURLToPath} from "url";
+import {createReadStream, createWriteStream} from "node:fs";
+
 const compress = async () => {
-    // Write your code here 
+    const sourcePath = fileURLToPath(new URL("./files/fileToCompress.txt", import.meta.url));
+    const outputPath = fileURLToPath(new URL("./files/archive.gz", import.meta.url));
+
+    const gzip = createGzip();
+    const readStream = createReadStream(sourcePath);
+    const writeStream = createWriteStream(outputPath);
+
+    readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import {fileURLToPath} from "url";
+import {createGunzip} from "node:zlib";
+import {createReadStream, createWriteStream} from "node:fs";
+
 const decompress = async () => {
-    // Write your code here 
+  const sourcePath = fileURLToPath(new URL("./files/archive.gz", import.meta.url));
+  const outputPath = fileURLToPath(new URL("./files/fileToCompress.txt", import.meta.url));
+
+  const gunzip = createGunzip();
+  const readStream = createReadStream(sourcePath);
+  const writeStream = createWriteStream(outputPath);
+
+  readStream.pipe(gunzip).pipe(writeStream);
 };
 
 await decompress();
